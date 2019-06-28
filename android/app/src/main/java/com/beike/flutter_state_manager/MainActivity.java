6f0ec01e5dda5a99541c393d8fbad04571abc9e0
zhaoyayu001 <zhaoyayu001@ke.com>
2019-06-28 12:11:42 +0800

    <ADD> add MainActivity.java

---
+++ android/app/src/main/java/com/beike/flutter_state_manager/MainActivity.java
@@ -0,0 +1,14 @@
+package com.beike.flutter_state_manager;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
+
